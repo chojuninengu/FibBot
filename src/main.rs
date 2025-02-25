7ef2cc9f3146ce1bc1af31d75212d1eb3f729dde
chojuninengu <chojuninengu@gmail.com>
2025-02-25 15:58:33 +0100

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
 }
+fn extract_numbers(text: &str) -> Vec<u64> {
+    text.split_whitespace()
+        .filter_map(|word| word.parse::<u64>().ok())
+        .collect()
+}
